306cb30f33b7696e42e38499e321f76142380d67
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-05-16 16:23:38 +0100

    Get the server to run local-only by default

--- serv/run_server.py
+++ serv/run_server.py
@@ -28,4 +28,4 @@ if __name__ == "__main__":
 
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=DEBUG, host="0.0.0.0", port=port)
+    app.run(debug=DEBUG, host="127.0.0.1", port=port)
