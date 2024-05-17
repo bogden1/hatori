92876a85040d7156b0d16df6cd54f86421252ce4
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-05-17 18:44:49 +0100

    Add capacity to drive asset regeneration from cli

--- serv/generate_assets.py
+++ serv/generate_assets.py
@@ -5,23 +5,26 @@ import math
 import numpy as np
 import src.generator
 import src.reader
-
-read = False
+import argparse
 
 def setseed():
     np.random.seed(0)
     random.seed(0)
 
+parser = argparse.ArgumentParser(description = '''Generates assets for the web app.
+Optionally regenerates the data from which those assets are generated. To do so, set both --state and --docs''')
+parser.add_argument('--state', '-s', help = 'Location of topic state file from Mallet, for example ../huge-data/debates/topic_state_500')
+parser.add_argument('--docs', '-d', help = 'Location of Mallet input text, for example ../huge-data/debates/mc-20170814-stemmed.txt')
+args = parser.parse_args()
 
-if read:
-    namestate = "../huge-data/debates/topic_state_500"
-    namedocs = "../huge-data/debates/mc-20170814-stemmed.txt"
-
-    data = src.reader.TextData(namestate, namedocs)
+if args.state and args.docs:
+    data = src.reader.TextData(args.state, args.docs)
     data.counts
     data.sorts
     data.sums
     data.save("data/data.p")
+elif args.state or args.docs:
+    print(f'Must give both or neither of --state and --docs.', file = sys.stderr)
 
 with open("data/data.p", "rb") as f:
     print("reading", flush=True)
