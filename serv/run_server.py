c7d33bca83566fb7b135823927c71e8bbda12db4
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2024-05-31 17:20:05 +0100

    Make back end host env-var configurable
    This makes Dockerisation easier, while still not exposing a port
    to the world by default.

--- serv/run_server.py
+++ serv/run_server.py
@@ -28,4 +28,5 @@ if __name__ == "__main__":
 
 
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=DEBUG, host="127.0.0.1", port=port)
+    host = os.environ.get('HOST', '127.0.0.1')
+    app.run(debug=DEBUG, host=host, port=port)
